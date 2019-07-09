10cf6d91b763dadf68c98a8176d3012c651abdb6
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-07-09 15:58:46 +0000

    simple form

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -4,4 +4,24 @@ class LotsController < ApplicationController
       @lots = Lot.all
       end
 
+    def new
+      @lot = Lot.new
+    end
+   
+
+      def create
+        @lot = Lot.create(lot_params)
+        if @lot.valid?
+           redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def lot_params
+        params.require(:lot).permit(:name, :address, :description)
+      end
+
 end
